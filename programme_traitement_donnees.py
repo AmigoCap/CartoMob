60089e255aaf03cc6d7fc1957041a81d68bec0c0
fablyon <33518019+fablyon@users.noreply.github.com>
2018-05-16 16:46:19 +0200

    Update programme_traitement_donnees.py

--- programme_traitement_donnees.py
+++ programme_traitement_donnees.py
@@ -16,8 +16,8 @@ for e in L:
     df = pd.read_json(f)
     donnee=df.get("locations")
     for i in range(len(donnee)):
-        la=donnee[i]["laitudeE7"]/1e7
-        lo=donnee[i]["loitudeE7"]/1e7
+        la=donnee[i]["latitudeE7"]/1e7
+        lo=donnee[i]["longitudeE7"]/1e7
         t=donnee[i]["timestampMs"]
         if (la>((0.41)*(lo)+43.8292) and la<((0.393)*(lo)+43.912) and la<(-1.25*(lo)+51.747) and la>(-81.5*(lo)+434.175)) or (la>((-1.1627)*(lo)+51.32236) and la<((-1.6666)*(lo)+53.7361) and la<(0.5699*(lo)+43.06686) and la>(0.611*(lo)+42.86844)) or (la>((-1.2778)*(lo)+51.879) and la<((-1.6)*(lo)+53.41658) and la<(0.175*(lo)+44.9496525) and la>(0.5*(lo)+43.3909)) or (la>((0.42857)*(lo)+43.74) and la<((0.375)*(lo)+43.9962) and la<(-1*(lo)+50.5508) and la>4.7762) or (la>((1.8333)*(lo)+37.0358) and la<((1.6923)*(lo)+37.71) and la<(-0.2*(lo)+46.73768) and la>(-1.66667*(lo)+46.576233)) or (la>((-0.3357)*(lo)+47.38) and la<((-0.575)*(lo)+48.523) and la<(0.6142857*(lo)+42.8528814) and la>(0.42*(lo)+43.778)) or (la>((-1.1)*(lo)+51.03143) and la<((-2.0667)*(lo)+55.64414) and la<(0.3125*(lo)+44.294) and la>(0.3*(lo)+44.35301)): 
             lat.append(la)
