4b7aabe4193d8ae7fcaae34e00a5b65d16c5e4bd
TheoBertrand11 <31959348+TheoBertrand11@users.noreply.github.com>
2018-05-30 15:49:09 +0200

    Add files via upload

--- programme_theo/main_comptage.py
+++ programme_theo/main_comptage.py
@@ -6,27 +6,28 @@ os.chdir('C:/Users/theot/Desktop/srcipt_pe')
 import decompte
 from loading_files import charger
 import zone
+
+from graphviz import Digraph
+import matplotlib.pyplot as plt
 import numpy as np
 from chali import affichage_jours,affichage_heure,annuaire_jours
 
 donnees=charger()
 
 
-data_list=[{'lat':[x[i]['latitudeE7']/1e7 for i in range(len(x))],'long':[x[i]['longitudeE7']/1e7 for i in range(len(x))],'temps':[x[i]['timestampMs'] for i in range(len(x))]} for x in donnees]
+data_list=[{'lat':[x[i]['latitudeE7']/1e7 for i in range(len(x))],'long':[x[i]['longitudeE7']/1e7 for i in range(len(x))],'temps':[int(x[i]['timestampMs'])/1000 for i in range(len(x))]} for x in donnees]
 
 
-
+##Fonctions qui permettent de mettre en forme le jeu de données, séparant heure par heure 
 def decoupage_heure(l):
     """on découpe sur la journée, les données d'une personne"""
-    temps=[(int(t)/1000) for t in l['temps']]
-    out=[affichage_heure(h,l['lat'],l['long'],temps) for h in range(24)]
+    out=[affichage_heure(h,l['lat'],l['long'],l['temps']) for h in range(24)]
     out_con=[{'lat':[q[0] for q in x], 'long':[q[1] for q in x], 'temps':[q[2] for q in x] } for x in out ]
     return(out_con)
 
 def decoupage_journee(l):
     """On découpe en journées"""
-    temps=[int(t)/1000 for t in l['temps']]
-    out=annuaire_jours(l['lat'],l['long'],temps)
+    out=annuaire_jours(l['lat'],l['long'],l['temps'])
     return out
 
 
@@ -35,6 +36,7 @@ def decoupage_tot(l):
     out=[decoupage_heure(a) for a in A]
     return(out)
 
+## Deux fonction utiles pour manier les dictionnaires
 def somme_dict(L):
     """prend en argument une liste de dictionnaires, avec tous les memes clés, et retourne la somme des valeurs associées à chaque clé"""
     S={key:0 for key in L[0].keys()}
@@ -44,7 +46,11 @@ def somme_dict(L):
 
 def sum_dict(dict):
     """fais la somme des valeurs dans un dictionnaire"""
-    return(sum(dict.values()))
+    s=sum(dict.values())
+    if s==0:
+        return 1
+    else:
+        return s
 
 def dict_zone(l):
     A=decoupage_tot(l)
@@ -52,34 +58,109 @@ def dict_zone(l):
         for j in range(len(A[i])):
             A[i][j]=zone.quelle_zone(A[i][j])
     return A
+# 
+# def tot_compte():
+#     L=[]
+#     for x in data_list:
+#         L.append(decoupage_tot(x))
+#     
+#     A=[[] for j in range(len(data_list[0]))]
+#     for i in range(len(L[0])):
+#         for j in range(len(L[0][0])):
+#             A[i].append(somme_dict([zone.quelle_zone(L[q][i][j]) for q in range(len(L))]))
+#     return A
+# 
 
-def tot_compte():
-    L=[]
-    for x in data_list:
-        L.append(decoupage_tot(x))
+def localisation_tot(jour,n):
+    L=[dict_zone(x) for x in data_list]
+    out=somme_dict([ x[jour][n] for x in L])
+    s=sum_dict(out)
+    if s!=0:
+        out={key:out[key]/s for key in out.keys()}    #renormalisation
+    return out
     
-    A=[[] for j in range(len(data_list[0]))]
-    for i in range(len(L[0])):
-        for j in range(len(L[0][0])):
-            A[i].append(somme_dict([zone.quelle_zone(L[q][i][j]) for q in range(len(L))]))
-    return A
+
+# pour afficher:
+# plt.bar(range(len(D)), list(D.values()), align='center')
+# plt.xticks(range(len(D)), list(D.keys()))
+
+
+def afficher_12h(jour):
+    Z=[localisation_tot(jour,8+h) for h in range(12)]
+    fig1, axes1 = plt.subplots(ncols=6, nrows=1, figsize=(13.42, 12))
+    for h in range(12):
+        axes1[h].bar(range(len(Z[h])), list(Z[h].values()), align='center')
+        axes1[h].set_xticklabels(list(Z[h].keys()), rotation='vertical')
+        
+    fig1, axes1 = plt.subplots(ncols=6, nrows=1, figsize=(13.42, 12))
+    for h in range(6):
+        axes[h].bar(range(len(Z[h])), list(Z[h].values()), align='center')
+        axes[h].set_xticklabels(list(Z[h].keys()), rotation='vertical')
+        
+    plt.show()
+    return None
+
+##Construction d'un graphe de changement d'état
+
+def decision_location(l):
+    """ l est le dictionnaire relatif à une heure en nombre de points par zone """
+    if sum_dict(l)==0:
+        return 'autre'
+    else:
+        return max(l, key=lambda k: l[k])
+
+def decision_journee(l):
+    out=[decision_location(x) for x in l]
+    return out
+
+def construction_matrice(l):
+    debaze={'zone_labo':0,'zone_W1':0,'zone_M':0,'zone_entree':0,'zone_residence':0,'entree_chemin_mouilles':0,'entree_charriere_blanche':0,'autre':0}
+    dict_trans={key:debaze.copy() for key in debaze.keys()}
+    
+    A=dict_zone(l)
+    
+    L=[decision_journee(x) for x in A]
+    
+    for x in L:
+        for i in range(23):
+            dict_trans[x[i]][x[i+1]]+=1
+    
+    return dict_trans
+
+def somme_dict_matrice(L):
+    """L une liste de matrices-dictionnaires de mêmes clés..."""
+    S={}
+    for key in L[0].keys():
+        S[key]=somme_dict([x[key] for x in L])
+    return S
+    
+def passage_tot():
+    M=somme_dict_matrice([construction_matrice(x) for x in data_list])
+    M={key1: {key2:M[key1][key2]/sum_dict(M[key1]) for key2 in M[key1].keys()} for key1 in M.keys()}
+    return M
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def graphe():
+    M=passage_tot()
+    
+    os.chdir('C:/Users/theot/Desktop/srcipt_pe/graff')
+    
+    G=Digraph(name='chgtdetat', engine='dot')
+    for x in M.keys():
+        G.node(x)
+    for x in M.keys():
+        for y in M.keys():
+            s=int(M[x][y]*100)/100
+            if s>0:
+                G.edge(x,y,str(s))
+                
+    G.view()
+    
+    # pour aller ouvrir un .gv déjà defini : graphviz.render('circo','png',os.getcwd() + '\\chgtdetat.gv')
+    
+    return None
+    
 
 
 
---
+++ programme_theo/zone.py
@@ -0,0 +1,77 @@
+"""
+Created on Wed Feb  7 15:51:49 2018
+@author: fabienlyon
+"""
+
+import json
+import pandas as pd
+import chali
+
+L=[]
+lat,long,temps=[],[],[]
+for e in L:
+    f=open(e)
+    df = pd.read_json(f)
+    donnee=df.get("locations")
+    for i in range(len(donnee)):
+        la=donnee[i]["latitudeE7"]/1e7
+        lo=donnee[i]["longitudeE7"]/1e7
+        t=donnee[i]["timestampMs"]
+        if (la>((0.41)*(lo)+43.8292) and la<((0.393)*(lo)+43.912) and la<(-1.25*(lo)+51.747) and la>(-81.5*(lo)+434.175)) or (la>((-1.1627)*(lo)+51.32236) and la<((-1.6666)*(lo)+53.7361) and la<(0.5699*(lo)+43.06686) and la>(0.611*(lo)+42.86844)) or (la>((-1.2778)*(lo)+51.879) and la<((-1.6)*(lo)+53.41658) and la<(0.175*(lo)+44.9496525) and la>(0.5*(lo)+43.3909)) or (la>((0.42857)*(lo)+43.74) and la<((0.375)*(lo)+43.9962) and la<(-1*(lo)+50.5508) and la>4.7762) or (la>((1.8333)*(lo)+37.0358) and la<((1.6923)*(lo)+37.71) and la<(-0.2*(lo)+46.73768) and la>(-1.66667*(lo)+46.576233)) or (la>((-0.3357)*(lo)+47.38) and la<((-0.575)*(lo)+48.523) and la<(0.6142857*(lo)+42.8528814) and la>(0.42*(lo)+43.778)) or (la>((-1.1)*(lo)+51.03143) and la<((-2.0667)*(lo)+55.64414) and la<(0.3125*(lo)+44.294) and la>(0.3*(lo)+44.35301)): 
+            lat.append(la)
+            long.append(lo)
+            temps.append(t)
+    f.close()
+
+
+
+def zone_labo(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone des laboratoires"""
+    return(lat>((0.41)*(long)+43.8292) and lat<((0.393)*(long)+43.912) and lat<(-1.25*(long)+51.747) and lat>(-81.5*(long)+434.175))
+
+def zone_W1(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone des bâtiments W1 et W1bis"""
+    return(lat>((-1.1627)*(long)+51.32236) and lat<((-1.6666)*(long)+53.7361) and lat<(0.5699*(long)+43.06686) and lat>(0.611*(long)+42.86844))
+
+def zone_M(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone des M14, M15, M16 et Gymnase"""
+    return(lat>((-1.2778)*(long)+51.879) and lat<((-1.6)*(long)+53.41658) and lat<(0.175*(long)+44.9496525) and lat>(0.5*(long)+43.3909)) 
+
+
+def zone_entree(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone de l'entrée principale"""
+    return(lat>((0.42857)*(long)+43.74) and lat<((0.375)*(long)+43.9962) and lat<(-1*(long)+50.5508) and lat>4.7762)
+
+def zone_residence(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone de l'entrée principale"""
+    return(lat>((1.8333)*(long)+37.0358) and lat<((1.6923)*(long)+37.71) and lat<(-0.2*(long)+46.73768) and lat>(-1.66667*(long)+46.576233)) 
+
+def entree_charriere_blanche(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone de l'entrée principale"""
+    return(lat>((-0.3357)*(long)+47.38) and lat<((-0.575)*(long)+48.523) and lat<(0.6142857*(long)+42.8528814) and lat>(0.42*(long)+43.778))
+
+def entree_chemin_mouilles(long,lat,temps):
+    """Renvoie l'ensemble des points dans la zone de l'entrée principale"""
+    return(lat>((-1.1)*(long)+51.03143) and lat<((-2.0667)*(long)+55.64414) and lat<(0.3125*(long)+44.294) and lat>(0.3*(long)+44.35301))
+    
+def quelle_zone(dict):
+    """Prends en argument un dictionnaire {'lat','long','temps'} et renvoie un dictionnaire détaillant le nombre de points dans chaque zone"""
+    out={'zone_labo':0,'zone_W1':0,'zone_M':0,'zone_entree':0,'zone_residence':0,'entree_chemin_mouilles':0,'entree_charriere_blanche':0,'autre':0}
+    for i in range(len(dict['lat'])):
+        if zone_W1(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['zone_W1']+=1
+        elif zone_residence(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['zone_residence']+=1
+        elif zone_labo(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['zone_labo']+=1
+        elif zone_entree(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['zone_entree']+=1
+        elif zone_M(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['zone_M']+=1
+        elif entree_charriere_blanche(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['entree_charriere_blanche']+=1
+        elif entree_chemin_mouilles(dict['long'][i],dict['lat'][i],dict['temps'][i]):
+            out['entree_chemin_mouilles']+=1
+        else:
+            out['autre']+=1
+    return out
